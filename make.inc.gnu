bd79549fa1c10ea4f61cde7d1ae095b167236f56
German Sinuco <gsinuco@gmail.com>
2020-09-14 19:11:21 +0100

    Checking out to review Timing branch

--- make.inc.gnu
+++ make.inc.gnu
@@ -16,7 +16,9 @@ CFLAGS = -O3
 #  and desired compiler options for your machine. 
 GF         = gfortran
 GFFLAGS    =  -llapack -lblas -g 
-BARRYFLAGS =  -g -fPIC	
+BARRYFLAGS =  -g -fPIC	# to generate object files that are boundled 
+                        # in a dynamic library
+
 #  Define LDFLAGS to the desired linker options for your machine.
 #
 LDFLAGS =
@@ -33,13 +35,13 @@ DYLIB_NAME = libmultimodefloquet.so
 #  Uncomment the next four command lines to include routines using the 
 #  MKL-intel library. Edited as needed for your system.
 ###
-BUILD_MKL = yes
+#BUILD_MKL = yes
 #=========== Set the MKL-intel LIBRARY PATH ======================
-MKLLIBS = /opt/intel/compilers_and_libraries/linux/mkl/lib/intel64
+#MKLLIBS = /opt/intel/compilers_and_libraries/linux/mkl/lib/intel64
 
 #=========== Set the MKL-intel INCLUDE PATH ======================
-MKLINC = /opt/intel/compilers_and_libraries/linux/mkl/include	
+#MKLINC = /opt/intel/compilers_and_libraries/linux/mkl/include	
 
 #=========== Set the MKL-intel libary flags ======================
-MKLFLAGS   =  -lmkl_gf_lp64 -lmkl_sequential -lmkl_core
+#MKLFLAGS   =  -lmkl_gf_lp64 -lmkl_sequential -lmkl_core
 
--- src/F_representation.f90
+++ src/F_representation.f90
@@ -5,46 +5,24 @@ SUBROUTINE F_representation(Fx,Fy,Fz,Ftotal)
   IMPLICIT NONE
   DOUBLE PRECISION, DIMENSION(:,:), INTENT(OUT):: Fx,Fy,Fz
   DOUBLE PRECISION, INTENT(IN) :: Ftotal
-  !INTEGER, INTENT(IN) :: Ftotal_
-
-  !DOUBLE PRECISION
   INTEGER k,p,N_k
-  double precision k_!,Ftotal
+  double precision k_
 
   Fx = 0.0
   Fy = 0.0 
   Fz = 0.0
-!  Ftotal = 1.0*Ftotal_
-!  write(*,*) "with F",ftotal
-!!$  N_k = 2*Ftotal + 1
-!!$  DO k=-int(Ftotal),int(Ftotal),1
-!!$     
-!!$     Fz(k+int(Ftotal) + 1,k+int(Ftotal) + 1) = 1.0*k
-!!$     
-!!$  END DO
-!!$
-!!$   DO k=-int(Ftotal),int(Ftotal),1
-!!$     DO p=-int(Ftotal),int(Ftotal),1
-!!$        
-!!$        Fx(k+int(Ftotal+1),p+int(Ftotal+1)) = 0.5*(SQRT(Ftotal*(Ftotal+1) - k*(k+1))*delta_kr_int(k+1,p)  +& 
-!!$             &  SQRT(Ftotal*(Ftotal+1) - k*(k-1))*delta_kr_int(k-1,p))
-!!$        Fy(k+int(Ftotal+1),p+int(Ftotal+1)) = 0.5*(SQRT(Ftotal*(Ftotal+1) - k*(k+1))*delta_kr_int(k+1,p)  -&
-!!$             & SQRT(Ftotal*(Ftotal+1) - k*(k-1))*delta_kr_int(k-1,p))
-!!$
-!!$     END DO
-!!$  END DO
+
   N_k = 2*Ftotal + 1
   k_ = -(N_k - 1.0)/2.0
  
-  DO k=1,N_k!-int(Ftotal),int(Ftotal),1
+  DO k=1,N_k
      Fz(k,k) = k_
      k_ = k_ + 1
   END DO
 
   k_ = -(N_k - 1.0)/2.0  
-  DO k=1,N_k!-int(Ftotal),int(Ftotal),1
-     DO p=1,N_k!-int(Ftotal),int(Ftotal),1
-!        write(*,*) k_,k,p,N_k,Ftotal
+  DO k=1,N_k
+     DO p=1,N_k
         Fx(k,p) = 0.5*(SQRT(Ftotal*(Ftotal+1) - k_*(k_+1))*delta_kr_int(k+1,p)  +& 
              &  SQRT(Ftotal*(Ftotal+1) - k_*(k_-1))*delta_kr_int(k-1,p))
         Fy(k,p) = 0.5*(SQRT(Ftotal*(Ftotal+1) - k_*(k_+1))*delta_kr_int(k+1,p)  -&
@@ -53,14 +31,5 @@ SUBROUTINE F_representation(Fx,Fy,Fz,Ftotal)
      END DO
      k_ = k_ + 1
   END DO
-!!$  do p=1,int(2*FTotal+1)
-!!$     write(*,*) (abs(Fy(p,k)), k=1,int(2*FTotal+1))
-!!$  end do
-!!$  write(*,*) 
-!!$  do p=1,int(2*FTotal+1)
-!!$     write(*,*) (abs(Fx(p,k)), k=1,int(2*FTotal+1))
-!!$  end do
-!!$  write(*,*)
 
- 205 FORMAT(5e15.6)
 END SUBROUTINE F_representation
